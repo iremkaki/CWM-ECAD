292a3232131cf795d5381e584ceb37ba25c36acc
Cloud User <centos@ip-172-31-11-0.eu-west-2.compute.internal>
2021-06-17 09:54:42 +0000

    Solution to Ex5

--- Ex5/top.v
+++ Ex5/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #5 - Air Conditioning
-// Student Name:
-// Date: 
+// Student Name: Irem Kaki
+// Date: 16/06/2021
 //
 //  Description: In this exercise, you need to an air conditioning control system
 //  According to the state diagram provided in the exercise.
@@ -13,3 +13,74 @@
 //           heating, cooling
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module ac (
+    input clk,
+    input [4:0] temperature,
+    output reg heating,
+    output reg cooling
+    );
+
+    //define the state - 00 idle, 01 heating ON, 10 cooling ON - start from idle
+    reg [1:0] state = 2'b00;
+
+    //define all possible moves between states
+    always @(posedge clk) begin
+        case(state)
+            //state is idle
+            2'b00: begin
+                //if <= 18, move to heating
+                if (temperature <= 18) begin
+                    heating = 1;
+                    cooling = 0;
+                    state = 2'b01;
+                end
+                //if >= 22, move to cooling
+                if (temperature >= 22) begin
+                    heating = 0;
+                    cooling = 1;
+                    state = 2'b10;                
+                end
+                //else, stay idle
+                else begin
+                    heating = 0;
+                    cooling = 0;
+                    state = 2'b00;
+                end
+            end
+            
+            //state is heating
+            2'b01: begin
+                //if >= 20, move to idle
+                if (temperature >= 20) begin
+                    heating = 0;
+                    cooling = 0;
+                    state = 2'b00;
+                end
+                //else, stay heating
+                else begin
+                    heating = 1;
+                    cooling = 0;
+                    state = 2'b01;                
+                end
+            end
+
+            //state is cooling
+            2'b10: begin
+                //if <= 20, move to idle
+                if (temperature <= 20) begin
+                    heating = 0;
+                    cooling = 0;
+                    state = 2'b00;
+                end
+                //else, stay cooling
+                else begin
+                    heating = 0;
+                    cooling = 1;
+                    state = 2'b10;
+                end
+            end
+        endcase
+    end
+endmodule

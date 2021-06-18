db8606bdc803c10cb2321ca930ecdc7eca26f63c
Cloud User <centos@ip-172-31-11-0.eu-west-2.compute.internal>
2021-06-18 16:09:12 +0000

    Stage 1 - problems heating and cooling in tb

--- Ex8/top.v
+++ Ex8/top.v
@@ -1,7 +1,8 @@
+
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #8  - Simple End-to-End Design
-// Student Name:
-// Date: 
+// Student Name: Irem Kaki
+// Date: 18/06/2021
 //
 //  Description: In this exercise, you need to design an air conditioning systems
 //
@@ -17,17 +18,26 @@ module top(
     input clk_p,
     input clk_n,
      //Todo: add all other ports besides clk_n and clk_p 
-   );
-    
+
+    input rst_n,
+    input temperature_0,
+    input temperature_1,
+    input temperature_2,
+    input temperature_3,
+    input temperature_4,
+    output heating,
+    output cooling
+    );
+
 
    /* clock infrastructure, do not modify */
         wire clk_ibufds;
 
     IBUFDS IBUFDS_sysclk (
-	.I(clk_p),
-	.IB(clk_n),
-	.O(clk_ibufds)
-);
+        .I(clk_p),
+        .IB(clk_n),
+        .O(clk_ibufds)
+    );
 
      wire clk; //use this signal as a clock for your design
         
@@ -36,6 +46,14 @@ module top(
 	.O  (clk)
       );
 
-//Add logic here
+    //Add logic here
+    wire [4:0] temperature = {temperature_4, temperature_3, temperature_2, temperature_1, temperature_0};
 
+    ac ac(
+        .clk (clk),
+        .temperature (temperature),
+        .heating (heating),
+        .cooling (cooling)
+        );
+    
 endmodule
--- Ex8/top_tb.v
+++ Ex8/top_tb.v
@@ -1,9 +1,96 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #8  - Simple End-to-End Design
-// Student Name:
-// Date: 
+// Student Name: Irem Kaki
+// Date: 18/06/2021
 //
 // Description: A testbench module to test Ex8
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb(
+    );
+
+    parameter CLK_PERIOD = 10;
+    reg clk_n;
+    reg clk_p;
+    reg rst_n;
+    reg [4:0] temperature;
+    wire heating;
+    wire cooling;
+    reg err;
+
+
+    // clock generation
+    initial begin
+        clk_n = 1'b0;
+        clk_p = 1'b0;
+        forever begin
+            #(CLK_PERIOD/2)
+            clk_n =~ clk_n;
+            clk_p =~ clk_p;            
+	    end  
+    end 
+
+
+    // set regular tests
+    initial begin
+        err = 0;
+        temperature = 16;
+        forever begin
+            #CLK_PERIOD
+            // cannot be cooling and heating at the same time
+            if ((cooling == 1) && (heating == 1)) begin
+                $display("Test failed - illegal state, heating and cooling at once");
+                err = 1;
+            end
+
+            // check idle state
+            if ((temperature > 18) && (temperature < 22) && (cooling != 0) && (heating != 0)) begin
+                $display("Test failed - error in idle state");
+                err = 1;
+            end 
+
+            // check heating state
+            if ((temperature <= 18) && (cooling != 0) && (heating != 1)) begin
+                $display("Test failed - error in heating state");
+                err = 1;
+            end
+
+            // check cooling state
+            if ((temperature >= 22) && (cooling != 1) && (heating != 0)) begin
+                $display("Test failed - error in cooling state");
+                err = 1;
+            end
+            #(CLK_PERIOD*3)
+            temperature = temperature + 1;
+        end
+    end
+
+
+    // finish test, check for error
+    initial begin
+        #(CLK_PERIOD*50)
+        if (err == 0) begin
+            $display("Test passed :)");
+        end
+        $finish;
+    end    
+
+    // instantiate module
+    top top (
+        .clk_p (clk_p),
+        .clk_n (clk_n),
+        .rst_n (rst_n),
+        .temperature_0 (temperature[0]),
+        .temperature_1 (temperature[1]),
+        .temperature_2 (temperature[2]),
+        .temperature_3 (temperature[3]),
+        .temperature_4 (temperature[4]),
+        .heating (heating),
+        .cooling (cooling)
+        );
+
+
+endmodule
